9cf94c71e5559ba7282335e2fc8ec35e0d012ce6
Sepehr Sameni <Sepehr.Sameni@gmail.com>
2018-06-02 16:51:13 +0430

    ICA, PCA

--- dataset.py
+++ dataset.py
@@ -4,24 +4,26 @@ import torch
 import math
 import os
 import glob
+from sklearn.decomposition import FastICA, PCA
 
 
 class MyDataset(Dataset):
-    def __init__(self, dir_path='../MADSYN/data/eeg', num_files=200, seq_len=256, stride=0.5,
-                 model_dataset_depth_offset=2,  # we start with 4x4 resolution instead of 1x1
-                 model_initial_depth=0, alpha=1.0, num_channels=1):
-        self.model_depth = model_initial_depth
-        self.alpha = alpha
+    def __init__(self, dir_path='../MADSYN/data/eeg', num_files=860, seq_len=256, stride=0.5,
+                 max_freq=80, num_channels=1, per_user=True, mode=None,  # it could be None, pca or ica
+                 use_abs=False, model_dataset_depth_offset=2):  # we start with 4x4 resolution instead of 1x1
+        self.model_depth = 0
+        self.alpha = 1.0
         self.model_dataset_depth_offset = model_dataset_depth_offset
         self.dir_path = dir_path
         self.all_files = glob.glob(os.path.join(dir_path, '*_1.txt'))[:num_files]
         self.seq_len = seq_len
         self.stride = int(seq_len * stride)
         self.num_channels = num_channels
+        self.use_abs = use_abs
         sizes = []
         for i in range(num_files):
             with open(self.all_files[i]) as f:
-                all_data_len = len(list(map(float, f.read().split())))
+                all_data_len = len(list(map(float, f.read().split()))) // (80 // max_freq)
                 sizes.append(max(int(np.ceil((all_data_len - self.seq_len + 1) / self.stride)), 0))
         self.sizes = sizes
         self.data_pointers = [(i, j) for i in range(num_files) for j in range(self.sizes[i])]
@@ -30,17 +32,56 @@ class MyDataset(Dataset):
         for i in range(num_files):
             for j in range(num_channels):
                 with open('{}_{}.txt'.format(self.all_files[i][:-6], j + 1)) as f:
-                    tmp = np.array(list(map(float, f.read().split())), dtype=np.float32)[:num_points[i]]
+                    tmp = np.array(list(map(float, f.read().split())), dtype=np.float32)[::(80 // max_freq)][
+                          :num_points[i]]
                     self.datas[i][j, :] = tmp
-                self.datas[i] = ((self.datas[i] - self.datas[i].min()) / (
-                            self.datas[i].max() - self.datas[i].min())) * 2.0 - 1.0
+            if per_user:
+                self.datas[i] = self.normalize(self.datas[i])
+        if not per_user:
+            self.normalize_all(num_files)
+        if mode:
+            self.transformer = PCA(n_components=num_channels) if mode == 'pca' else FastICA(n_components=num_channels)
+            self.transformer.fit(np.concatenate([d.T for d in self.datas], axis=0))
+            for i in range(num_files):
+                self.datas[i] = self.transformer.transform(self.datas[i].T).T
+            self.all_max, self.all_min = self.normalize_all(num_files)
+        else:
+            self.transformer = None
         self.max_dataset_depth = self.infer_max_dataset_depth(self.load_file(0))
         self.min_dataset_depth = self.model_dataset_depth_offset
         self.description = {
             'len': len(self),
-            'shape': 'unknown',
-            'depth_range': ('unknown', self.max_dataset_depth)
+            'shape': self.shape,
+            'depth_range': (self.min_dataset_depth, self.max_dataset_depth)
         }
+
+    def inverse_transform(self, x):
+        if self.transformer is None:
+            return x
+        y = self.denormalize(x)
+        return np.stack([self.transformer.inverse_transform(y[i].T).T for i in range(len(y))])
+
+    def denormalize(self, x):
+        if self.use_abs:
+            return x * max(abs(self.all_max), abs(self.all_min))
+        return (x + 1.0) / 2.0 * (self.all_max - self.all_min) + self.all_min
+
+    def normalize_all(self, num_files):
+        all_max = max([self.datas[i].max() for i in range(num_files)])
+        all_min = min([self.datas[i].min() for i in range(num_files)])
+        for i in range(num_files):
+            self.datas[i] = self.normalize(self.datas[i], all_max, all_min)
+        return all_max, all_min
+
+    def normalize(self, arr, arr_max=None, arr_min=None):
+        if arr_max is None:
+            arr_max = arr.max()
+        if arr_min is None:
+            arr_min = arr.min()
+        if self.use_abs:
+            arr_max = max(abs(arr_max), abs(arr_min))
+            return arr / arr_max
+        return ((arr - arr_min) / (arr_max - arr_min)) * 2.0 - 1.0
 
     @property
     def data(self):
@@ -80,7 +121,8 @@ class MyDataset(Dataset):
         return torch.from_numpy(datapoint.astype('float32'))
 
     def alpha_fade(self, datapoint):
+        if self.alpha == 1:
+            return datapoint
         c, t = datapoint.shape
         t = datapoint.reshape(c, t // 2, 2).mean(axis=2).repeat(2, 1)
-        datapoint = (datapoint + (t - datapoint) * (1 - self.alpha))
-        return datapoint
+        return datapoint + (t - datapoint) * (1 - self.alpha)
--- generate.py
+++ generate.py
@@ -1,24 +1,25 @@
 from torch.autograd import Variable
-from utils import *
+from utils import cudize, random_latents, generic_arg_parse, generate_samples, get_structured_params
+import torch
 from argparse import ArgumentParser
 from functools import partial
+import pickle
+import glob
 
 default_params = {
     'generator_path': '',
-    'num_samples': 6,
-    'description': 'unknown',
+    'num_samples': 128,
+    'output_path': '',
+    'is_dir': False
 }
 
 
-def output_samples(generator_path, num_samples, description):
+def output_samples(generator_path, num_samples):
     G = torch.load(generator_path, map_location=lambda storage, location: storage)
     G = cudize(G)
-    latent_size = getattr(G, 'latent_size', 512)  # yup I just want to use old checkpoints
-    print('Sampling noise...')
+    latent_size = getattr(G, 'latent_size', 512)
     gen_input = cudize(Variable(random_latents(num_samples, latent_size)))
-    print('Generating...')
     output = generate_samples(G, gen_input)
-    print('Done.')
     return output
 
 
@@ -28,6 +29,11 @@ if __name__ == '__main__':
         parser.add_argument('--{}'.format(k), type=partial(generic_arg_parse, hinttype=type(default_params[k])))
     parser.set_defaults(**default_params)
     params = get_structured_params(vars(parser.parse_args()))
-    xx = output_samples(params['generator_path'], params['num_samples'], params['description'])
-    import pickle
-    pickle.dump(xx, open('data.gen', 'wb'))
+    if params['is_dir']:
+        params['generator_path'] = params['generator_path'] + '*'
+        for generator in glob.glob(params['generator_path']):
+            xx = output_samples(generator, params['num_samples'])
+            pickle.dump(xx, open(generator.replace('.dat', '.pkl'), 'wb'))
+    else:
+        xx = output_samples(params['generator_path'], params['num_samples'])
+        pickle.dump(xx, open(params['generator_path'], 'wb'))
