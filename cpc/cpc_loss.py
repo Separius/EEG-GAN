correct cpc prediction calculation

cpc/cpc_loss.py
@@ -186,17 +186,18 @@ class SeqOneMI(nn.Module):
 
 class KPredLoss(nn.Module):
     def __init__(self, c_size, z_size, k=4, measure='JSD', mode='bilinear',
-                 auto_is_bidirectional=True, look_both=False):
+                 auto_is_bidirectional=True, look_both=False, cross_entropy=False):
         super().__init__()
-        assert k > 0
+        if k <= 0:
+            k = 0
         self.k = k
+        self.cross_entropy = cross_entropy
         self.split_c = auto_is_bidirectional and not look_both
         self.bidirectional = auto_is_bidirectional
         measure = measure.upper()
         mode = mode.lower()
         self.measure = measure
         self.mode = mode
-
         c_size = (c_size // 2) if self.split_c else c_size
         if self.mode == 'bilinear':
             self.forward_weights = []
@@ -219,6 +220,16 @@ class KPredLoss(nn.Module):
         positive_mask[b, :] = 1.0
         return calc_one_way_loss(u, positive_mask, mean_dim, self.measure)
 
+    @staticmethod
+    def calc_cross_entropy_loss(u, softmax_dim, target_id):
+        if softmax_dim == 0:
+            _u = u.permute(1, 0, 2)
+        elif softmax_dim == 2:
+            _u = u.permute(0, 2, 1)
+        else:
+            _u = u
+        return F.cross_entropy(_u, (torch.ones(_u.size(0), _u.size(2)) * target_id).long().to(u.device))
+
     def calc_both_way_loss_t(self, c_flat, z_flat, weight):
         t = z_flat.size(2)
         t = np.random.randint(t)
@@ -227,7 +238,10 @@ class KPredLoss(nn.Module):
         else:
             u = torch.einsum('bzt,dz->btd', c_flat, z_flat[..., t])
         acc1 = (u.argmax(dim=1) == t).sum().item() / (u.size(0) * u.size(1))
-        first_loss = self.calc_one_way_loss_t(u, t, torch.zeros(u.size(0), u.size(1)).to(u), 2)
+        if self.cross_entropy:
+            first_loss = self.calc_cross_entropy_loss(u, 1, t)
+        else:
+            first_loss = self.calc_one_way_loss_t(u, t, torch.zeros(u.size(0), u.size(1)).to(u), 2)
         t = c_flat.size(2)
         t = np.random.randint(t)
         if self.mode == 'bilinear':
@@ -235,7 +249,11 @@ class KPredLoss(nn.Module):
         else:
             u = torch.einsum('bz,dzt->bdt', c_flat[..., t], z_flat)
         acc2 = (u.argmax(dim=2) == t).sum().item() / (u.size(1) * u.size(2))
-        return first_loss + self.calc_one_way_loss_t(u, t, torch.zeros(u.size(1), u.size(2)).to(u), 0), (acc1, acc2)
+        if self.cross_entropy:
+            second_loss = self.calc_cross_entropy_loss(u, 2, t)
+        else:
+            second_loss = self.calc_one_way_loss_t(u, t, torch.zeros(u.size(1), u.size(2)).to(u), 0)
+        return first_loss + second_loss, (acc1, acc2)
 
     def calc_both_way_loss_b(self, c_flat, z_flat, weight):
         b = z_flat.size(0)
@@ -245,7 +263,10 @@ class KPredLoss(nn.Module):
         else:
             u = torch.einsum('bzt,zi->bti', c_flat, z_flat[b])
         acc1 = (u.argmax(dim=0) == b).sum().item() / (u.size(0) * u.size(1))
-        first_loss = self.calc_one_way_loss_b(u, b, torch.zeros(u.size(0), u.size(1)).to(u), 2)
+        if self.cross_entropy:
+            first_loss = self.calc_cross_entropy_loss(u, 0, b)
+        else:
+            first_loss = self.calc_one_way_loss_b(u, b, torch.zeros(u.size(0), u.size(1)).to(u), 2)
         b = c_flat.size(0)
         b = np.random.randint(b)
         if self.mode == 'bilinear':
@@ -253,14 +274,29 @@ class KPredLoss(nn.Module):
         else:
             u = torch.einsum('zt,dzi->tdi', c_flat[b], z_flat)
         acc2 = (u.argmax(dim=1) == b).sum().item() / (u.size(1) * u.size(2))
-        return first_loss + self.calc_one_way_loss_b(u, b, torch.zeros(u.size(1), u.size(2)).to(u), 0), (acc1, acc2)
+        if self.cross_entropy:
+            second_loss = self.calc_cross_entropy_loss(u, 1, b)
+        else:
+            second_loss = self.calc_one_way_loss_b(u, b, torch.zeros(u.size(1), u.size(2)).to(u), 0)
+        return first_loss + second_loss, (acc1, acc2)
 
     def calc_four_way_loss(self, c_flat, z_flat, weight):
         t_loss, t_acc = self.calc_both_way_loss_t(c_flat, z_flat, weight)
         b_loss, b_acc = self.calc_both_way_loss_b(c_flat, z_flat, weight)
         return t_loss + b_loss, t_acc, b_acc
 
+    def calc_full_cross_entropy_loss(self, c_flat, z_flat, weights):
+        c_flat = c_flat.contiguous().view(-1, c_flat.size(1))
+        z_flat = z_flat.contiguous().view(-1, z_flat.size(1))
+        u = torch.einsum('bc,cz,dz->bd', c_flat, weights, z_flat)
+        targets = torch.arange(u.size(0)).to(u.device)
+        acc = (u.argmax(1) == targets).sum().item() / u.size(0)
+        f_loss = F.cross_entropy(u, targets)
+        return f_loss, (acc, acc), (acc, acc)
+
     def forward(self, c, z):
+        if self.k == 0:
+            return torch.tensor(0.0).to(c), {}
         B, c_size, T = c.size()
         if self.split_c:
             c_size = c_size // 2
@@ -269,15 +305,20 @@ class KPredLoss(nn.Module):
         for i in range(self.k):
             c_flat = c[:, :c_size, :-(i + 1)]
             z_flat = z[..., (i + 1):]
-            f_loss, t_acc, b_acc = self.calc_four_way_loss(c_flat, z_flat, self.forward_weights[i])
+            if self.cross_entropy:  # this is the safe version
+                f_loss, t_acc, b_acc = self.calc_full_cross_entropy_loss(c_flat, z_flat, self.forward_weights[i])
+            else:
+                f_loss, t_acc, b_acc = self.calc_four_way_loss(c_flat, z_flat, self.forward_weights[i])
             accs['f_{}_t'.format(i + 1)] = t_acc
             accs['f_{}_b'.format(i + 1)] = b_acc
             total_loss = total_loss + f_loss
-        if self.bidirectional:
-            for i in range(self.k):
+            if self.bidirectional:
                 c_flat = c[:, -c_size:, (i + 1):]
                 z_flat = z[..., :-(i + 1)]
-                f_loss, t_acc, b_acc = self.calc_four_way_loss(c_flat, z_flat, self.backward_weights[i])
+                if self.cross_entropy:  # this is the safe version
+                    f_loss, t_acc, b_acc = self.calc_full_cross_entropy_loss(c_flat, z_flat, self.forward_weights[i])
+                else:
+                    f_loss, t_acc, b_acc = self.calc_four_way_loss(c_flat, z_flat, self.backward_weights[i])
                 accs['b_{}_t'.format(i + 1)] = t_acc
                 accs['b_{}_b'.format(i + 1)] = b_acc
                 total_loss = total_loss + f_loss

cpc/cpc_network.py
@@ -6,7 +6,6 @@ import torch.nn.functional as F
 from torch.nn.utils import spectral_norm
 from pytorch_pretrained_bert.modeling import BertLayer
 
-from utils import cudize
 from cpc.cpc_loss import KPredLoss, OneOneMI, SeqOneMI
 
 
@@ -88,6 +87,7 @@ class ConvEncoder(nn.Module):
     def __init__(self, input_channels=5, long=True, use_pooling=False, activation='relu',
                  dropout=0.1, use_sinc=False, shared_sinc=True, tiny=False):
         super().__init__()
+        # TODO add residual connections?
         activation = activation.lower()
         if long:  # generates 32 codes of size 128
             down_ratios = [5, 4, 3]
@@ -225,7 +225,7 @@ class Transformer(nn.Module):
             self.backward_transformer = None
 
     def forward(self, x):  # BCT
-        pos_embedding = self.pos_embedding(cudize(torch.arange(x.size(2)))).permute(1, 0).unsqueeze(0)
+        pos_embedding = self.pos_embedding(torch.arange(x.size(2)).to(x.device)).permute(1, 0).unsqueeze(0)
         forward_mask = self.create_attention_mask(x.size(2), True).to(x)
         forward_output = (x + pos_embedding).permute(0, 2, 1)
         for bl in self.forward_transformer:
@@ -245,12 +245,15 @@ class Network(nn.Module):
     def __init__(self, input_channels, generate_long_sequence=True, pooling=False, encoder_dropout=0.1,
                  use_sinc_encoder=False, use_shared_sinc=True, bidirectional=False, contextualizer_num_layers=1,
                  contextualizer_dropout=0, use_transformer=False, causal_prediction=True, prediction_k=4,
-                 encoder_activation='relu', tiny_encoder=True):
+                 encoder_activation='relu', tiny_encoder=True, have_global=True, have_local=True, cross_entropy=True):
         super().__init__()
         encoder = ConvEncoder(input_channels=input_channels, long=generate_long_sequence, use_pooling=pooling,
                               activation=encoder_activation, dropout=encoder_dropout, use_sinc=use_sinc_encoder,
                               shared_sinc=use_shared_sinc, tiny=tiny_encoder)
-        z_pooler = PNormPooling(encoder.z_size)
+        if have_global:
+            z_pooler = PNormPooling(encoder.z_size)
+        else:
+            z_pooler = None
         if use_transformer:
             contextualizer = Transformer(input_size=encoder.z_size, causal=True, bidirectional=bidirectional,
                                          num_heads=4, max_seq_len=32 if generate_long_sequence else 16,
@@ -259,12 +262,23 @@ class Network(nn.Module):
             contextualizer = AutoRNN(input_size=encoder.z_size, hidden_size=2 * encoder.z_size,
                                      bidirectional=bidirectional, num_layers=contextualizer_num_layers,
                                      dropout=contextualizer_dropout, cell_type='GRU')
-        c_pooler = PNormPooling(contextualizer.c_size)
+        if have_global or have_local:
+            c_pooler = PNormPooling(contextualizer.c_size)
+        else:
+            c_pooler = None
         prediction_loss_network = KPredLoss(contextualizer.c_size, encoder.z_size, k=prediction_k,
-                                            auto_is_bidirectional=bidirectional, look_both=not causal_prediction)
-        mi_hidden_size = min(c_pooler.pool_size, z_pooler.pool_size) * 2
-        c_pooled_mi_z_pooled = OneOneMI(c_pooler.pool_size, z_pooler.pool_size, hidden_size=mi_hidden_size)
-        c_pooled_mi_z = SeqOneMI(c_pooler.pool_size, encoder.z_size, hidden_size=mi_hidden_size)
+                                            cross_entropy=cross_entropy, auto_is_bidirectional=bidirectional,
+                                            look_both=not causal_prediction)
+        if have_global:
+            c_pooled_mi_z_pooled = OneOneMI(c_pooler.pool_size, z_pooler.pool_size,
+                                            hidden_size=min(c_pooler.pool_size, z_pooler.pool_size) * 2)
+        else:
+            c_pooled_mi_z_pooled = None
+        if have_local:
+            c_pooled_mi_z = SeqOneMI(c_pooler.pool_size, encoder.z_size,
+                                     hidden_size=min(c_pooler.pool_size, encoder.z_size) * 2)
+        else:
+            c_pooled_mi_z = None
 
         self.encoder = encoder
         self.z_pooler = z_pooler
@@ -274,29 +288,41 @@ class Network(nn.Module):
         self.c_pooled_mi_z_pooled = c_pooled_mi_z_pooled
         self.c_pooled_mi_z = c_pooled_mi_z
 
-    def forward(self, x):
+    def forward(self, x, return_all=False):
         z = self.encoder(x)
-        z_pooled = self.z_pooler(z)
+        if self.z_pooler is not None:
+            z_pooled = self.z_pooler(z)
+        else:
+            z_pooled = z.mean(dim=2)
         c = self.contextualizer(z)
-        c_pooled = self.c_pooler(c)
+        if self.c_pooler is not None:
+            c_pooled = self.c_pooler(c)
+        else:
+            c_pooled = c.mean(dim=2)
         prediction_loss, pred_acc = self.prediction_loss_network(c, z)
-        global_discriminator_loss, global_accuracy = self.c_pooled_mi_z_pooled(c_pooled, z_pooled)
-        local_discriminator_loss, local_accuracy = self.c_pooled_mi_z(c_pooled, z)
-        return prediction_loss, global_discriminator_loss, local_discriminator_loss, c_pooled, global_accuracy, local_accuracy, pred_acc
+        if self.c_pooled_mi_z_pooled is not None:
+            global_discriminator_loss, global_accuracy = self.c_pooled_mi_z_pooled(c_pooled, z_pooled)
+        else:
+            global_discriminator_loss, global_accuracy = torch.tensor(0.0).to(c_pooled), [0.0, 0.0]
+        if self.c_pooled_mi_z is not None:
+            local_discriminator_loss, local_accuracy = self.c_pooled_mi_z(c_pooled, z)
+        else:
+            local_discriminator_loss, local_accuracy = torch.tensor(0.0).to(c_pooled), [0.0, 0.0]
+        return_values = prediction_loss, global_discriminator_loss, local_discriminator_loss, \
+                        global_accuracy, local_accuracy, pred_acc
+        if return_all:
+            return return_values, z, c, z_pooled, c_pooled
+        return return_values
 
     def inference_forward(self, x):
         z = self.encoder(x)
-        z_pooled = self.z_pooler(z)
+        if self.z_pooler is not None:
+            z_pooled = self.z_pooler(z)
+        else:
+            z_pooled = z.mean(dim=2)
         c = self.contextualizer(z)
-        c_pooled = self.c_pooler(c)
+        if self.c_pooler is not None:
+            c_pooled = self.c_pooler(c)
+        else:
+            c_pooled = c.mean(dim=2)
         return z, c, z_pooled, c_pooled
-
-    def complete_forward(self, x):
-        z = self.encoder(x)
-        z_pooled = self.z_pooler(z)
-        c = self.contextualizer(z)
-        c_pooled = self.c_pooler(c)
-        prediction_loss, pred_acc = self.prediction_loss_network(c, z)
-        global_discriminator_loss, global_accuracy = self.c_pooled_mi_z_pooled(c_pooled, z_pooled)
-        local_discriminator_loss, local_accuracy = self.c_pooled_mi_z(c_pooled, z)
-        return prediction_loss, global_discriminator_loss, local_discriminator_loss, c_pooled, global_accuracy, local_accuracy, pred_acc, z, c, z_pooled

cpc/cpc_train.py
@@ -10,13 +10,14 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, SubsetRandomSampler
 
 hp = AttrDict(
-    generate_long_sequence=True,
-    pool_or_stride='stride',
+    generate_long_sequence=True,  # 32 vs 16
+    pool_or_stride='stride',  # max pooling tends to overfit
     use_shared_sinc=True,
-    seed=12658,
+    seed=12658,  # used when val=0
     prediction_k=4,
-    validation_ratio=0.1,
-    ds_stride=0.5,
+    cross_entropy=True,
+    validation_ratio=0,
+    ds_stride=1,
     num_channels=17,
     use_sinc_encoder=False,
     causal_prediction=True,
@@ -33,18 +34,23 @@ hp = AttrDict(
 
     contextualizer_num_layers=1,
     contextualizer_dropout=0,
-    encoder_dropout=0.1,
+    encoder_dropout=0.2,  # used to be 0.1
     encoder_activation='relu',  # glu or relu
     tiny_encoder=False,
 
     batch_size=128,
     lr=2e-3,
-    epochs=32,
+    epochs=31,
     weight_decay=0.005,
 )
 
 
 def main(summary):
+    # TODO make these function
+    # TODO better encoder (residual)
+    # TODO better(cross entropy based) loss for global?
+    # TODO better(cross entropy based) loss for local??
+    # TODO have two flags for the cross entropy in prediction
     train_dataset, val_dataset = ThinEEGDataset.from_config(validation_ratio=hp.validation_ratio,
                                                             num_channels=hp.num_channels, stride=hp.ds_stride)
     if hp.validation_ratio == 0:
@@ -68,8 +74,10 @@ def main(summary):
                              use_sinc_encoder=hp.use_sinc_encoder, use_shared_sinc=hp.use_shared_sinc,
                              bidirectional=hp.bidirectional, contextualizer_num_layers=hp.contextualizer_num_layers,
                              contextualizer_dropout=hp.contextualizer_dropout, use_transformer=hp.use_transformer,
-                             causal_prediction=hp.causal_prediction, prediction_k=hp.prediction_k,
-                             encoder_activation=hp.encoder_activation, tiny_encoder=hp.tiny_encoder))
+                             causal_prediction=hp.causal_prediction, have_global=(hp.global_loss_weight != 0.0),
+                             prediction_k=hp.prediction_k * (hp.prediction_loss_weight != 0.0),
+                             encoder_activation=hp.encoder_activation, tiny_encoder=hp.tiny_encoder,
+                             have_local=(hp.local_loss_weight != 0.0), cross_entropy=hp.cross_entropy))
     num_parameters = num_params(network)
     print('num_parameters', num_parameters)
     if hp.use_bert_adam:
@@ -83,6 +91,8 @@ def main(summary):
     for epoch in range(hp.epochs):
         for training, data_loader in zip((False, True), (val_dataloader, train_dataloader)):
             if training:
+                if epoch == hp.epochs - 1:
+                    break
                 network.train()
             else:
                 network.eval()
@@ -99,7 +109,7 @@ def main(summary):
             with torch.set_grad_enabled(training):
                 for batch in data_loader:
                     x = cudize(batch)
-                    prediction_loss, global_discriminator_loss, local_discriminator_loss, c_pooled, global_accuracy, local_accuracy, pred_accuracy = network(
+                    prediction_loss, global_discriminator_loss, local_discriminator_loss, global_accuracy, local_accuracy, pred_accuracy = network(
                         x)
                     global_accuracy_one, global_accuracy_two = global_accuracy
                     local_accuracy_one, local_accuracy_two = local_accuracy
@@ -133,11 +143,13 @@ def main(summary):
             total_local_discriminator_loss /= total_count
             total_local_accuracy = (total_local_accuracy_one + total_local_accuracy_two) / 2
             total_network_loss /= total_count
-
-            metrics = dict(prediction_loss=total_prediction_loss, prediction_acc=total_pred_acc,
-                           global_loss=total_global_discriminator_loss, global_acc=total_global_accuracy,
-                           local_loss=total_local_discriminator_loss, local_acc=total_local_accuracy,
-                           net_loss=total_network_loss)
+            metrics = dict(net_loss=total_network_loss)
+            if network.prediction_loss_network.k > 0:
+                metrics.update(dict(prediction_loss=total_prediction_loss, prediction_acc=total_pred_acc))
+            if hp.global_loss_weight != 0:
+                metrics.update(dict(global_loss=total_global_discriminator_loss, global_acc=total_global_accuracy))
+            if hp.local_loss_weight != 0:
+                metrics.update(dict(local_loss=total_local_discriminator_loss, local_acc=total_local_accuracy))
             if not training and hp.use_scheduler:
                 scheduler.step(metrics['net_loss'])
             if summary:
@@ -151,4 +163,4 @@ def main(summary):
 
 
 if __name__ == '__main__':
-    main(summary=True)
+    main(summary=False)