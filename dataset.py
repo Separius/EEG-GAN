18e377713e840fc788093fef8616956622008292
Sepehr Sameni <Sepehr.Sameni@gmail.com>
2019-04-21 21:25:36 +0430

    dataset v7 is here! :))

--- dataset.py
+++ dataset.py
@@ -1,7 +1,5 @@
 import os
 import glob
-from random import shuffle
-
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -11,21 +9,13 @@ from torch.utils.data.dataloader import default_collate
 
 from utils import load_pkl, save_pkl, resample_signal, cudize, random_latents
 
-DATASET_VERSION = 6
+DATASET_VERSION = 7
 
 '''
 !pip install imageio
 !pip install gdown
-#val=0.1, stride=0.5, num_channels=17
-!gdown --id 17-KHzOzejlbtfyTDa9goPiETxesVFKDy
-!gdown --id 1kqU1fFINqknRyIyxw9ameGdi4LJzqE-s
-!gdown --id 1g6363MyMk0pSgwdPN-wW4mFjI98HKmEM
-!gdown --id 1lW4IyO-dmjzTgTmvTvJNJrzLllxj-W2e
-#val=0, stride=1, num_channels=17
+#val=0, stride=1(1.5), num_channels=17, start=1
 !gdown --id 1Vw4sQDRyAEjnl1lH77D8PVkba954BvGG
-!gdown --id 1kv6w38Oyo3-PyaKqOEmSVYaKSc-DOZid
-!gdown --id 1xCNE-zIV6fMFPSxRye1V8X7BVwgS_tYF
-!gdown --id 14vvrnu8LfJy_BAwWRvAFzrLR8zPqv2uv
 '''
 
 
@@ -42,13 +32,10 @@ class EEGDataset(Dataset):
 
     picked_channels = [3, 5, 9, 15, 16]
 
-    def __init__(self, train_files, norms, given_data, validation_ratio: float = 0.1,
-                 dir_path: str = './data/prepared_eegs_mat_th5/', data_sampling_freq: float = 220,
-                 start_sampling_freq: float = 1, end_sampling_freq: float = 60, start_seq_len: int = 32,
-                 stride: float = 0.5, num_channels: int = 17, number_of_files: int = 100000,
-                 per_user_normalization: bool = True, per_channel_normalization: bool = False):
+    def __init__(self, given_data=None, dir_path='./data/prepared_eegs_mat_th5/', data_sampling_freq=220,
+                 start_sampling_freq=1, end_sampling_freq=60, start_seq_len=32, num_channels=17, return_long=False):
         super().__init__()
-        self.model_depth = 0
+        self.model_depth = len(self.progression_scale_up)
         self.alpha = 1.0
         self.dir_path = dir_path
         self.end_sampling_freq = end_sampling_freq
@@ -57,31 +44,19 @@ class EEGDataset(Dataset):
         seq_len = int(seq_len)
         self.seq_len = seq_len
         self.initial_kernel_size = start_seq_len
-        self.stride = int(seq_len * stride)
-        self.per_user_normalization = per_user_normalization
-        self.per_channel_normalization = per_channel_normalization
+        self.stride = int(seq_len * 1.5)
         self.max_dataset_depth = len(self.progression_scale_up)
-        self.norms = norms
         self.num_channels = num_channels if self.picked_channels is None else len(self.picked_channels)
+        self.return_long = return_long
         if given_data is not None:
-            self.sizes = given_data[0]['sizes']
-            self.files = given_data[0]['files']
-            self.norms = given_data[0]['norms']
-            self.data_pointers = given_data[0]['pointers']
-            self.datas = [given_data[1]['arr_{}'.format(i)] for i in range(len(given_data[1].keys()))]
+            self.data_pointers = given_data[0]
+            self.datas = given_data[1]
             return
-        all_files = glob.glob(os.path.join(dir_path, '*_1.txt'))[:number_of_files]
+        all_files = glob.glob(os.path.join(dir_path, '*_1.txt'))
         is_matlab = len(all_files) == 0
         if is_matlab:
-            all_files = glob.glob(os.path.join(dir_path, '*.mat'))[:number_of_files]
-        files = len(all_files)
-        files = [i for i in range(files)]
-        if train_files is None:
-            shuffle(files)
-            files = files[:int(len(all_files) * (1.0 - validation_ratio))]
-        else:
-            files = list(set(files) - set(train_files))
-        self.files = files
+            all_files = glob.glob(os.path.join(dir_path, '*.mat'))
+        files = [i for i in range(len(all_files))]
         sizes = []
         num_points = []
         self.datas = []
@@ -120,79 +95,49 @@ class EEGDataset(Dataset):
                             self.datas.append(np.zeros((num_channels, num_points[-1]), dtype=np.float32))
                         tmp = tmp[:num_points[-1]]
                         self.datas[-1][j, :] = tmp
-            if is_ok and per_user_normalization:
-                self.datas[-1], is_ok = self.normalize(self.datas[-1], self.per_channel_normalization)
+            if is_ok:
+                self.datas[-1], is_ok = self.normalize(self.datas[-1])
                 if not is_ok:
                     del sizes[-1]
                     del num_points[-1]
                     del self.datas[-1]
-        self.sizes = sizes
-        self.data_pointers = [(i, j) for i, s in enumerate(self.sizes) for j in range(s)]
-        if not per_user_normalization:
-            self.normalize_all()
+        self.data_pointers = [(i, j) for i, s in enumerate(sizes) for j in range(s)]
 
     @classmethod
-    def from_config(cls, validation_ratio: float, dir_path: str, number_of_files: int,
-                    data_sampling_freq: float, start_sampling_freq: float, end_sampling_freq: float,
-                    start_seq_len: int, stride: float, num_channels: int,
-                    per_user_normalization: bool, per_channel_normalization: bool):
+    def from_config(cls, validation_ratio, validation_seed: float, dir_path, data_sampling_freq,
+                    start_sampling_freq, end_sampling_freq, start_seq_len, num_channels, return_long):
         assert end_sampling_freq <= data_sampling_freq
-        mode = per_user_normalization * 2 + per_channel_normalization * 1
-        train_files = None
-        train_norms = None
-        datasets = [None, None]
-        for index, split in enumerate(('train', 'val')):
-            target_location = os.path.join(dir_path, '{}%_{}c_{}m_{}s_{}v_{}ss_{}es_{}l_{}n_{}.npz'
-                                           .format(validation_ratio, num_channels, mode, stride,
-                                                   DATASET_VERSION, start_sampling_freq, end_sampling_freq,
-                                                   start_seq_len, number_of_files, split))
-            given_data = None
-            if os.path.exists(target_location):
-                print('loading {} dataset from file'.format(split))
-                if split == 'val' and validation_ratio == 0.0:
-                    print('creating {} dataset from scratch'.format(split))
-                else:
-                    given_data = (load_pkl(target_location + '.pkl'), np.load(target_location))
-            else:
-                print('creating {} dataset from scratch'.format(split))
-            dataset = cls(train_files, train_norms, given_data, validation_ratio, dir_path, data_sampling_freq,
-                          start_sampling_freq, end_sampling_freq, start_seq_len, stride, num_channels,
-                          number_of_files, per_user_normalization, per_channel_normalization)
-            if train_files is None:
-                train_files = dataset.files
-                train_norms = dataset.norms
-            if given_data is None:
-                np.savez_compressed(target_location, *dataset.datas)
-                save_pkl(target_location + '.pkl', {'sizes': dataset.sizes, 'pointers': dataset.data_pointers,
-                                                    'norms': dataset.norms, 'files': dataset.files})
-            datasets[index] = dataset
-        return datasets[0], datasets[1]
-
-    def normalize_all(self):
-        num_files = len(self.datas)
-        if self.norms is None:
-            all_max = np.max(
-                np.array([data.max(axis=1 if self.per_channel_normalization else None) for data in self.datas]), axis=0)
-            all_min = np.min(
-                np.array([data.min(axis=1 if self.per_channel_normalization else None) for data in self.datas]), axis=0)
-            self.norms = (all_max, all_min)
+        target_location = os.path.join(dir_path, '{}c_{}v_{}ss_{}es_{}l.npz'.format(num_channels, DATASET_VERSION,
+                                                                                    start_sampling_freq,
+                                                                                    end_sampling_freq, start_seq_len))
+        if os.path.exists(target_location):
+            print('loading dataset from file: {}'.format(target_location))
+            given_data = np.load(target_location)
+            given_data = [load_pkl(target_location + '.pkl'), [given_data['arr_{}'.format(i)]
+                                                               for i in range(len(given_data.keys()))]]
         else:
-            all_max, all_min = self.norms
-        is_ok = True
-        for i in range(num_files):
-            self.datas[i], is_ok = self.normalize(self.datas[i], self.per_channel_normalization, all_max, all_min)
-        if not is_ok:
-            raise ValueError('data is constant!')
+            print('creating dataset from scratch')
+            dataset = cls(None, dir_path, data_sampling_freq, start_sampling_freq,
+                          end_sampling_freq, start_seq_len, num_channels, return_long)
+            np.savez_compressed(target_location, *dataset.datas)
+            save_pkl(target_location + '.pkl', dataset.data_pointers)
+            given_data = [dataset.data_pointers, dataset.datas]
+        return_datasets = []
+        for i in range(2):
+            return_datasets.append(cls(given_data, dir_path, data_sampling_freq, start_sampling_freq,
+                                       end_sampling_freq, start_seq_len, num_channels, return_long))
+        data_pointers = [x for x in return_datasets[0].data_pointers]
+        np.random.seed(validation_seed)
+        np.random.shuffle(data_pointers)
+        return_datasets[0].data_pointers = data_pointers[:int((1 - validation_ratio) * len(data_pointers))]
+        return_datasets[1].data_pointers = data_pointers[int((1 - validation_ratio) * len(data_pointers)):]
+        return return_datasets[0], return_datasets[1]
 
     @staticmethod
-    def normalize(arr, per_channel, arr_max=None, arr_min=None):
-        if arr_max is None:
-            arr_max = arr.max(axis=1 if per_channel else None)
-        if arr_min is None:
-            arr_min = arr.min(axis=1 if per_channel else None)
+    def normalize(arr):
+        arr_max = arr.max()
+        arr_min = arr.min()
         is_ok = arr_max != arr_min
-        if per_channel:
-            is_ok = is_ok.all()
         return ((arr - arr_min) / ((arr_max - arr_min) if is_ok else 1.0)) * 2.0 - 1.0, is_ok
 
     @property
@@ -204,8 +149,11 @@ class EEGDataset(Dataset):
 
     def load_file(self, item):
         i, k = self.data_pointers[item]
-        res = self.datas[i][:, k * self.stride:k * self.stride + self.seq_len]
-        return res
+        if self.return_long:
+            return self.datas[i][:, k * self.stride:k * self.stride + self.stride]
+        else:
+            rand_shift = np.random.randint(self.stride - self.seq_len)
+            return self.datas[i][:, k * self.stride + rand_shift:k * self.stride + rand_shift + self.seq_len]
 
     def resample_data(self, data, index, forward=True, alpha_fade=False):
         up_scale = self.progression_scale_up[index - (1 if alpha_fade else 0)]
@@ -251,61 +199,3 @@ def get_collate_fake(latent_size, z_distribution, collate_real):
         return batch
 
     return collate_fake
-
-
-class ThinEEGDataset(Dataset):
-    def __init__(self, given_data, data_sampling_freq, start_seq_len, stride):
-        super().__init__()
-        seq_len = start_seq_len * data_sampling_freq
-        assert seq_len == int(seq_len), 'seq_len must be an int'
-        seq_len = int(seq_len)
-        self.seq_len = seq_len
-        self.stride = int(seq_len * stride)
-        if given_data is not None:
-            self.sizes = given_data[0]['sizes']
-            self.files = given_data[0]['files']
-            self.data_pointers = given_data[0]['pointers']
-            self.datas = [given_data[1]['arr_{}'.format(i)] for i in range(len(given_data[1].keys()))]
-            self.num_channels = self.datas[0].shape[0]
-            return
-        else:
-            raise ValueError('This is the Thin Wrapper and can only load data')
-
-    @classmethod
-    def from_config(cls, validation_ratio: float = 0, dir_path: str = '.', number_of_files: int = 100000,
-                    start_sampling_freq: float = 1, end_sampling_freq: float = 60, start_seq_len: int = 32,
-                    stride: float = 0.8, num_channels: int = 5, per_user_normalization: bool = True,
-                    per_channel_normalization: bool = False):
-        mode = per_user_normalization * 2 + per_channel_normalization * 1
-        datasets = [None, None]
-        for index, split in enumerate(('train', 'val')):
-            target_location = os.path.join(dir_path, '{}%_{}c_{}m_{}s_{}v_{}ss_{}es_{}l_{}n_{}.npz'
-                                           .format(validation_ratio, num_channels, mode, stride,
-                                                   DATASET_VERSION, start_sampling_freq, end_sampling_freq,
-                                                   start_seq_len, number_of_files, split))
-            given_data = None
-            if os.path.exists(target_location):
-                print('loading {} dataset from file'.format(split))
-                if split == 'val' and validation_ratio == 0:
-                    return datasets[0], None
-                else:
-                    given_data = (load_pkl(target_location + '.pkl'), np.load(target_location))
-            else:
-                raise ValueError('Can\'t read ' + target_location)
-            dataset = cls(given_data, end_sampling_freq, start_seq_len, stride)
-            datasets[index] = dataset
-        return datasets[0], datasets[1]
-
-    @property
-    def shape(self):
-        return len(self), self.num_channels, self.seq_len
-
-    def __len__(self):
-        return len(self.data_pointers)
-
-    def load_file(self, item):
-        i, k = self.data_pointers[item]
-        return self.datas[i][:, k * self.stride:k * self.stride + self.seq_len]
-
-    def __getitem__(self, item):
-        return torch.from_numpy(self.load_file(item).astype(np.float32))
